22eacc87479395d9adc4fbf0d58123409582078f
Elrey Belmonti and Evelyn Fenner-Dorrity <elrey@>
2017-08-15 14:41:01 -0700

    remove all vowels from users input

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
     console.log(phrase)
     for (var index = 0; index <= phrase.length-1; index++){
       console.log(phrase[index]);
-      if (phrase[index] === "a"){
+      if (phrase[index] === "a" || phrase[index] === "A"){
         puzzle += "-";
-      // } else if (phrase[index] === "e" || phrase[index] === "E"){
-      //   puzzle += "-";
-      // }else if (phrase[index] === "i" || phrase[index] === "I"){
-      //   puzzle += "-";
-      // }else if (phrase[index] === "o" || phrase[index] === "O"){
-      //   puzzle += "-";
-      // }else if (phrase[index] === "u" || phrase[index] === "U"){
-      //   puzzle += "-";
+      } else if (phrase[index] === "e" || phrase[index] === "E"){
+        puzzle += "-";
+      }else if (phrase[index] === "i" || phrase[index] === "I"){
+        puzzle += "-";
+      }else if (phrase[index] === "o" || phrase[index] === "O"){
+        puzzle += "-";
+      }else if (phrase[index] === "u" || phrase[index] === "U"){
+        puzzle += "-";
       } else {
         puzzle += phrase[index];
       }
