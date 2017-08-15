0f63863c7dbe8000be12e60f2131194e14b6ddcf
Elrey Belmonti and Evelyn Fenner-Dorrity <elrey@>
2017-08-15 14:38:58 -0700

    add logic to remove lower case a from the users input

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var puzzle = "";
+
+$(document).ready(function(){
+
+  $("#phrase").submit(function(event){
+    event.preventDefault();
+    var phrase = $("#sentence").val().split("");
+    console.log(phrase)
+    for (var index = 0; index <= phrase.length-1; index++){
+      console.log(phrase[index]);
+      if (phrase[index] === "a"){
+        puzzle += "-";
+      // } else if (phrase[index] === "e" || phrase[index] === "E"){
+      //   puzzle += "-";
+      // }else if (phrase[index] === "i" || phrase[index] === "I"){
+      //   puzzle += "-";
+      // }else if (phrase[index] === "o" || phrase[index] === "O"){
+      //   puzzle += "-";
+      // }else if (phrase[index] === "u" || phrase[index] === "U"){
+      //   puzzle += "-";
+      } else {
+        puzzle += phrase[index];
+      }
+    }
+    console.log(puzzle)
+  });
+});
